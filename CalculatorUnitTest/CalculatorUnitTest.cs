b2f7d73172e5c1a5a23dfab09f5e9b75f104f560
giovannitamburini <giovatambu@gmail.com>
2023-09-25 14:48:59 +0200

    created CalculatorUnitTest class and added the link to the main project

---
+++ CalculatorUnitTest/CalculatorUnitTest.cs
@@ -0,0 +1,16 @@
+namespace CalculatorUnitTest
+{
+    public class CalculatorUnitTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

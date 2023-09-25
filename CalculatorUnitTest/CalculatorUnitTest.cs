e3d2302ec0c0117f53f4753f134d6a58db86a1b2
giovannitamburini <giovatambu@gmail.com>
2023-09-25 15:53:01 +0200

    added test of divide

--- CalculatorUnitTest/CalculatorUnitTest.cs
+++ CalculatorUnitTest/CalculatorUnitTest.cs
@@ -18,13 +18,13 @@ namespace CalculatorUnitTest
             float num2 = 3f;
 
             // dichiaro output atteso
-            float total = 8f;
+            float totalExpected = 8f;
 
             // richiamo la funzione
             float outputAdd = Calculator.Add(num1, num2);
 
             // confronto output funzione con l'output atteso
-            Assert.IsTrue(outputAdd == total, $"Il risultato atteso sarebbe {total} mentre il risultato ottenuto è {outputAdd}");
+            Assert.IsTrue(outputAdd == totalExpected, $"Il risultato atteso dall'addizione sarebbe {totalExpected} mentre il risultato ottenuto è {outputAdd}");
         }
 
         [Test]
@@ -35,13 +35,32 @@ namespace CalculatorUnitTest
             float num2 = 3f;
 
             // dichiaro output atteso
-            float total = 2f;
+            float totalExpected = 2f;
 
             // richiamo la funzione
             float outputAdd = Calculator.Subtract(num1, num2);
 
             // confronto output funzione con l'output atteso
-            Assert.IsTrue(outputAdd == total, $"Il risultato atteso sarebbe {total} mentre il risultato ottenuto è {outputAdd}");
+            Assert.IsTrue(outputAdd == totalExpected, $"Il risultato atteso dalla sottrazione sarebbe {totalExpected} mentre il risultato ottenuto è {outputAdd}");
+        }
+
+        [Test]
+        public void TestDivide()
+        {
+            // dichiaro gli input
+            float num1 = 10f;
+            float num2 = 2f;
+
+            // dichiaro output atteso
+            float totalExpected = 5f;
+
+            // richiamo la funzione
+            float outputDivide = Calculator.Divide(num1, num2);
+
+            // confront output funzione con l'output atteso
+            Assert.IsTrue(outputDivide == totalExpected, $"Il risultato atteso dalla divisione sarebbe {totalExpected} mentre il risultato ottenuto è {outputDivide}");
+
+
         }
     }
 }

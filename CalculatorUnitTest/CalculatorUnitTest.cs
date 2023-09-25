3b5f989d0bb1e631780e3f3249cab6272030e025
giovannitamburini <giovatambu@gmail.com>
2023-09-25 15:44:13 +0200

    added test of subtract

--- CalculatorUnitTest/CalculatorUnitTest.cs
+++ CalculatorUnitTest/CalculatorUnitTest.cs
@@ -26,5 +26,22 @@ namespace CalculatorUnitTest
             // confronto output funzione con l'output atteso
             Assert.IsTrue(outputAdd == total, $"Il risultato atteso sarebbe {total} mentre il risultato ottenuto è {outputAdd}");
         }
+
+        [Test]
+        public void TestSubtract()
+        {
+            // dichiaro gli input
+            float num1 = 5f;
+            float num2 = 3f;
+
+            // dichiaro output atteso
+            float total = 2f;
+
+            // richiamo la funzione
+            float outputAdd = Calculator.Subtract(num1, num2);
+
+            // confronto output funzione con l'output atteso
+            Assert.IsTrue(outputAdd == total, $"Il risultato atteso sarebbe {total} mentre il risultato ottenuto è {outputAdd}");
+        }
     }
 }
